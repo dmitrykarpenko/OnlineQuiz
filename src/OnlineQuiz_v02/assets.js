84fca40f80f7d0f5c7777dcf5ad23e9abf99e179
Dmytro Karpenko <dmitry.karpenko@gmail.com>
2017-11-05 22:33:38 +0200

    webpack.config.js added

---
+++ src/OnlineQuiz_v02/assets.js
@@ -0,0 +1,8 @@
+﻿const CSSs = [   
+];
+const JSs = [
+    'jquery-validation/dist/jquery.validate.js',
+    'jquery-validation-unobtrusive/jquery.validate.unobtrusive.js',
+];
+
+module.exports = [...JSs, ...CSSs];
